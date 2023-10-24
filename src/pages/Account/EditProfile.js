03831c726e0930e23ec25112527fca87c909af41
@mawar.theresa <mawar.theresa@mantisworks.id>
2023-10-24 09:23:36 +0700

    add useeffect

--- src/pages/Account/EditProfile.js
+++ src/pages/Account/EditProfile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { ChevronLeft } from "react-feather";
 import { Button } from "reactstrap";
 import { useNavigate } from "react-router-dom";
